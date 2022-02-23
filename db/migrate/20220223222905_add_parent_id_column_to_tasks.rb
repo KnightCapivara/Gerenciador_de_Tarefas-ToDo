63e7e02e0b2e7ffb7a473587c2a2ad726dd51046
Danillo <danillo.targino@gmail.com>
2022-02-23 18:45:32 -0400

    update db

--- db/migrate/20220223222905_add_parent_id_column_to_tasks.rb
+++ db/migrate/20220223222905_add_parent_id_column_to_tasks.rb
@@ -1,4 +1,4 @@
-class AddParentIdColumnToTasks < ActiveRecord::Migration[5.2]
+class AddParentIdColumnToTasks < ActiveRecord::Migration[7.0]
   def change
     add_reference :tasks, :parent, foreign_key: { to_table: :tasks }
   end
